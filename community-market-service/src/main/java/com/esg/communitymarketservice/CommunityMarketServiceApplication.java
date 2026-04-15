05beba29685b765c79025480ed03db47b31809d5
dmstj0322 <dmstj5121777@gmail.com>
2026-04-16 01:20:33 +0900

    docker 구축 및 gitignore 설정 완료

--- community-market-service/src/main/java/com/esg/communitymarketservice/CommunityMarketServiceApplication.java
+++ community-market-service/src/main/java/com/esg/communitymarketservice/CommunityMarketServiceApplication.java
@@ -2,7 +2,9 @@ package com.esg.communitymarketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CommunityMarketServiceApplication {
 
