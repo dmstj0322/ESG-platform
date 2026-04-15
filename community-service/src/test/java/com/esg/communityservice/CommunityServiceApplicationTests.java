475703ce4113d661b7d02029523683c9b5ce9136
dmstj0322 <dmstj5121777@gmail.com>
2026-04-16 02:32:18 +0900

    community-market 서비스 분리

--- community-service/src/test/java/com/esg/communityservice/CommunityServiceApplicationTests.java
+++ community-service/src/test/java/com/esg/communityservice/CommunityServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.esg.communitymarketservice;
+package com.esg.communityservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CommunityMarketServiceApplicationTests {
+class CommunityServiceApplicationTests {
 
   @Test
   void contextLoads() {
