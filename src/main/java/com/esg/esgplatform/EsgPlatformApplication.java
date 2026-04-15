92fc33f2f4287a7e0970af9268c4423c6c0dc813
dmstj0322 <dmstj5121777@gmail.com>
2026-04-15 14:10:46 +0900

    프로젝트 생성

---
+++ src/main/java/com/esg/esgplatform/EsgPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.esg.esgplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgPlatformApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EsgPlatformApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/esg/esgplatform/EsgPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esg.esgplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgPlatformApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
