e6b0986dcf683d378cd1eced7ece8b8263ebdb23
dmstj0322 <dmstj5121777@gmail.com>
2026-04-15 15:50:28 +0900

    빌드 성공!!!

---
+++ community-market-service/src/main/java/com/esg/communitymarketservice/CommunityMarketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.esg.communitymarketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityMarketServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CommunityMarketServiceApplication.class, args);
+  }
+
+}
---
+++ community-market-service/src/test/java/com/esg/communitymarketservice/CommunityMarketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esg.communitymarketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityMarketServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
